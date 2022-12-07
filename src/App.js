04cf00f2b8ccdbae46c0d71d5907e252eafcd1a1
Dahlia <rlally89@gmail.com>
2022-12-07 14:08:47 -0800

    updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import Footer from './components/Footer';
-import Header from './components/Header'
+import PageContainer from './components/PageContainer'
 
 function App() {
   return (
     <>
-      <Footer />
+      <PageContainer />
     </>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import '../styles/Footer.css';
 function Footer() {
     return (
         <>
-
+           
         </>
     )
 }
