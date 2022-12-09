febdc3cdcad58cd583ff269dc9054f254c9c98f3
Dahlia <rlally89@gmail.com>
2022-12-08 16:05:31 -0800

    kitchen api route

--- src/components/pages/Kitchen.js
+++ src/components/pages/Kitchen.js
@@ -1,22 +1,40 @@
 import '../../styles/Kitchen.css';
 import API from '../../utils/API'
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
-function Kitchen() {
-// const [kitchen, setKitchen] = ([])
 
-// useEffect(()=>{
-//     API.getKitchens(props.userId).then(data=>{
-//         console.log(data)
-//         setKitchen(data.Todos)
-//     })
-// },[props.userId])
-  
+function Kitchen(props) {
+    const [kitchen, setKitchen] = useState([])
+
+    useEffect(() => {
+        API.getKitchens(props.userId).then(data => {
+            // const allKitchens = data.map((a,i)=>{
+            //     return a.Storages;
+            // })
+            const userKitchens = data.filter((k) => {
+                if (k.UserId === props.userId) {
+                    return k;
+                } else {  
+                return null;
+                }
+            }) 
+            // const stringKitchen = JSON.stringify(userKitchens)
+            console.log(data)
+            setKitchen(userKitchens)
+            console.log(userKitchens)
+ 
+        })
+    }, [props.userId])
+
     return (
         <div>
             <h1>Welcome to your Kitchen Placeholder</h1>
-            
-          
+            <div>
+                {kitchen.map((k,i)=>
+                <p key={i}> {k.zipCode}</p>)}
+                {/* {kitchen}  */}
+            </div>
+
         </div>
     )
 }
