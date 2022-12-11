2015e4ec46446d9fb199b0b867648da93881cd41
Dahlia <rlally89@gmail.com>
2022-12-10 21:48:15 -0800

    another commit whoops

--- src/components/pages/Recipe.js
+++ src/components/pages/Recipe.js
@@ -21,8 +21,8 @@ function Recipe() {
             <input 
                 
                 className="form-control block px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" 
-                // value={signupUserName} 
-                // onChange={e => setSignupUserName(e.target.value)} 
+                // value={} 
+                // onChange={e => (e.target.value)} 
                 placeholder="Ingredient" />
         </div>
     )
