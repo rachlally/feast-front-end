0e0eb044e61b18a8fab59d278c00325d3a9b89f8
Dahlia <rlally89@gmail.com>
2022-12-11 16:31:39 -0800

    edamame recipe api console log

--- src/components/pages/Recipe.js
+++ src/components/pages/Recipe.js
@@ -1,39 +1,21 @@
-import React, {useEffect} from 'react'
-// import '../styles/Recipe.css';
+import React from 'react'
+
+var userInput = "banana"
+
+fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${userInput}&app_id=f8d2a9ae&app_key=4d3ee4a8bbd450583932d553443686b8`)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
 
 function Recipe() {
-
-    getRecipe = (e) => {
-        e.preventDefault();
-        console.log("Working")
-    }
-//     const [ingredientSearch, setRecipe] = useState([''])
-
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': 'ec9e8aa11bmsh01afb5356f556dep1d5473jsn3185f0830ed0',
-// 		'X-RapidAPI-Host': 'edamam-food-and-grocery-database.p.rapidapi.com'
-// 	}
-// };
-
-// useEffect(()=>{
-//     fetch('https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr=' + ingredientSearch , options)
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
-// 	.catch(err => console.error(err));
-//     setRecipe()
-
-// }, []);
-
     return (
         <div>
             <h1>Cooking Inspiration</h1>
             <input 
-                type='text'
+                
                 className="form-control block px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" 
                 // value={} 
-                onChange={e => (e.target.value)} 
+                // onChange={e => (e.target.value)} 
                 placeholder="Ingredient" />
         </div>
     )
