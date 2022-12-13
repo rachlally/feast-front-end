78054fc177b666a590ff71e920a1f10c774813f7
andrew <ammassey42@gmail.com>
2022-12-12 16:00:16 -0800

    start adding api calls

--- src/components/pages/KitchenById.js
+++ src/components/pages/KitchenById.js
@@ -1,17 +1,25 @@
 import React, { useState, useEffect } from "react";
+import API from "../../utils/API";
 
 // console.log(window.location.href)
 
+
+
+
+function KitchenById(props) {
 const currentURL = window.location.href;
 const currentKitchenId = currentURL.substring(currentURL.lastIndexOf('/')+1)
 console.log(currentKitchenId)
 
+API.getOneKitchen(props.token, currentKitchenId).then((data) => {
+  console.log(data);
+});
 
-function KitchenById(props) {
+
   // console.log(storage)
   return (
-
     <>
+    
     <h1>PLEASE WORK</h1>
     </>
   )
