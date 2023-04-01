79e667c46601fb8606b601c610cf497c9634fe82
NathanAlexander1 <72030740+NathanAlexander1@users.noreply.github.com>
2023-04-01 16:34:45 -0700

    Change map center icon

--- src/components/pages/Map.js
+++ src/components/pages/Map.js
@@ -64,7 +64,7 @@ function Map({ lat, lng, foodBanks }) {
   }
 
   let iconMarker= new window.google.maps.MarkerImage(
-    "https://www.clipartmax.com/png/small/5-55328_leader-person-icon.png",
+    "https://lh3.googleusercontent.com/bECXZ2YW3j0yIEBVo92ECVqlnlbX9ldYNGrCe0Kr4VGPq-vJ9Xncwvl16uvosukVXPfV=w300",
     null, /* size is determined at runtime */
     null, /* origin is 0,0 */
     null, /* anchor is bottom center of the scaled image */
