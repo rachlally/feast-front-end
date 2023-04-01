7955710e698040ff30b0b35d2cf215fef736133c
NathanAlexander1 <72030740+NathanAlexander1@users.noreply.github.com>
2023-04-01 10:00:25 -0700

    Remove autocomplete

--- src/components/pages/Map.js
+++ src/components/pages/Map.js
@@ -5,10 +5,8 @@ import React, { useState, useMemo } from "react";
 import {
   GoogleMap,
   useLoadScript,
-  LoadScript,
   Marker,
   Autocomplete,
-  SearchBox,
   MarkerClusterer,
 } from "@react-google-maps/api";
 // import { MarkerClusterer } from "@googlemaps/markerclusterer";
@@ -34,17 +32,11 @@ function MapInit({ lat, lng, foodBanks }) {
 
 function Map({ lat, lng, foodBanks }) {
   // console.log(lat, lng);
-  // let autocomplete;
   // console.log(foodBanks);
-  const [autocomplete, setautocomplete] = useState(null);
+
   const [centerLatCoordinates, setCenterLatCoordinates] = useState(lat);
   const [centerLongCoordinates, setCenterLongCoordinates] = useState(lng);
 
-  function onLoad(a) {
-    // console.log("autocomplete: ", a);
-
-    setautocomplete(a);
-  }
   let center = useMemo(
     () => ({ lat: centerLatCoordinates, lng: centerLongCoordinates }),
     []
@@ -52,41 +44,6 @@ function Map({ lat, lng, foodBanks }) {
   let searchLat;
   let searchLong;
 
-  //onPlaceChange may not be necessary anymore since we have the maps rendering how we want it.
-  function onPlaceChanged() {
-    console.log(autocomplete);
-    console.log(autocomplete.gm_accessors_.place.jj.predictions);
-    if (autocomplete !== null) {
-      console.log(autocomplete.getPlace());
-      //search coordinates logging
-      console.log(
-        (autocomplete.getPlace().geometry.viewport.Wa.hi +
-          autocomplete.getPlace().geometry.viewport.Wa.lo) /
-          2
-      );
-      searchLat =
-        (autocomplete.getPlace().geometry.viewport.Wa.hi +
-          autocomplete.getPlace().geometry.viewport.Wa.lo) /
-        2;
-      //search coordinates logging
-      console.log(
-        (autocomplete.getPlace().geometry.viewport.Ia.hi +
-          autocomplete.getPlace().geometry.viewport.Ia.lo) /
-          2
-      );
-      searchLong =
-        (autocomplete.getPlace().geometry.viewport.Ia.hi +
-          autocomplete.getPlace().geometry.viewport.Ia.lo) /
-        2;
-      console.log(autocomplete.getPlace().formatted_address);
-
-      setCenterLatCoordinates(searchLat);
-      setCenterLongCoordinates(searchLong);
-      console.log(centerLatCoordinates, centerLongCoordinates);
-    } else {
-      console.log("Autocomplete is not loaded yet!");
-    }
-  }
   center = useMemo(
     () => ({ lat: centerLatCoordinates, lng: centerLongCoordinates }),
     [centerLatCoordinates, centerLongCoordinates]
@@ -105,43 +62,24 @@ function Map({ lat, lng, foodBanks }) {
   function createKey(location) {
     return location.lat + location.lng;
   }
-  
+
   return (
     <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
       <Marker
         position={{ lat: centerLatCoordinates, lng: centerLongCoordinates }}
       />
 
-      <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
-        <input
-          type="text"
-          placeholder="Search for a place"
-          style={{
-            boxSizing: `border-box`,
-            border: `1px solid transparent`,
-            width: `200px`,
-            height: `32px`,
-            padding: `0 12px`,
-            borderRadius: `3px`,
-            boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-            fontSize: `14px`,
-            outline: `none`,
-            textOverflow: `ellipses`,
-            position: "absolute",
-            left: "50%",
-            top: "90%",
-            marginLeft: "-120px",
-          }}
-        />
-      </Autocomplete>
-
       <MarkerClusterer markerImage={markerImage}>
-          {(clusterer) =>
-            locations.map((location) => (
-              <Marker key={createKey(location)} position={location} clusterer={clusterer} />
-            ))
-          }
-        </MarkerClusterer>
+        {(clusterer) =>
+          locations.map((location) => (
+            <Marker
+              key={createKey(location)}
+              position={location}
+              clusterer={clusterer}
+            />
+          ))
+        }
+      </MarkerClusterer>
     </GoogleMap>
   );
 }
